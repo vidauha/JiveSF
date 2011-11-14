a10836fa047ce08f2bd607ca52247cf492e05763
macadmin <macadmin@unknownf81edfe703c3.gateway.2wire.net>
2011-11-13 20:34:42 -0800

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JiveSF::Application.config.secret_token = '41e7c9647affa0af647c5605e3551d04254b31aad82f4866b902809fb6310d06ff0ea2fd86ac9c79f09a35a3fc073a879d08bdf49e29dd67354639047ba5e335'
